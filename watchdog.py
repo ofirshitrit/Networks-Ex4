20226454eda8421d3408ca3ec44d9f18b6f0085a
ofirshitrit <ofirha18@gmail.com>
2023-05-22 19:05:54 +0300

    the better version

--- watchdog.py
+++ watchdog.py
@@ -1,22 +1,43 @@
-import os
-import time
 import socket
+import time
 
-def watchdog_timer():
-    timeout = 10  # 10 seconds
-    start_time = time.time()
+def start_watchdog():
+    timeout = 10  # seconds
+
+    # Create a socket with TCP protocol
+    watchdog_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    watchdog_socket.bind(("localhost", 3000))
+    watchdog_socket.listen(1)
+
+    print("Watchdog started. Listening on port 3000...")
 
     while True:
-        elapsed_time = time.time() - start_time
-        if elapsed_time > timeout:
-            print(f"Server {host} cannot be reached.")
-            os._exit(0)  # Exit the program
+        # Accept incoming connection
+        client_socket, _ = watchdog_socket.accept()
+        start_time = time.time()
 
-        time.sleep(0.001)
+        while True:
+            try:
+                data = client_socket.recv(1024).decode()
+                if not data:
+                    break
+            except socket.error:
+                break
 
+            elapsed_time = time.time() - start_time
+
+            if elapsed_time > timeout:
+                print("Timeout occurred. Closing connection...")
+                client_socket.close()
+                break
+
+            if data == "Reply arrived":
+                print(f"Reply received in {elapsed_time:.2f} seconds")
+                start_time = time.time()
+
+        client_socket.close()
+
+    watchdog_socket.close()
 
 if __name__ == "__main__":
-    host = "google.com"
-
-    # Create and start the watchdog timer thread
-    watchdog_timer()
+    start_watchdog()
